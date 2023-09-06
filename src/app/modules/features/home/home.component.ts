a0ac0aadb9ba186dc8ecab0ac83a8d642e77a665
FacuMozo <facumozo98@gmail.com>
2023-09-06 16:33:26 -0300

    Add edit dialog

--- src/app/modules/features/home/home.component.ts
+++ src/app/modules/features/home/home.component.ts
@@ -5,6 +5,9 @@ import { Project } from '../../models/cproject.model';
 import { Story } from '../../models/cstory.model';
 import { MatDialog } from '@angular/material/dialog';
 import { StoryFormComponent } from '../story-form/story-form.component';
+import { LoadingDialogComponent } from '../../shared/loading-dialog/loading-dialog.component';
+import { ProjectDialogComponent } from '../../shared/project-dialog/project-dialog.component';
+import { DialogNotificationComponent } from '../../shared/dialog-notification/dialog-notification.component';
 
 @Component({
   selector: 'app-home',
@@ -66,17 +69,62 @@ export class HomeComponent implements OnInit {
   }
 
   updateStory(story : Story){
-    let dialogRef = this.dialog.open(StoryFormComponent,{
-      data : {name : story.name,
-         description : story.description,
-          epic : story.epic, 
-          sprint: story.sprint, 
-          owner : story.owner,
-          assignedTo : story.assignedTo, 
-          points : story.points,created: 
-          story.created, due :story.due, 
-          start :story.started, 
-          end :story.finished, status : story.status, option : 'Edit'}
+      let dialogRef = this.dialog.open(StoryFormComponent, {
+        data: { name: story.name, description: story.description, epic: story.epic, sprint: story.sprint, owner: story.owner, assignedTo: story.assignedTo, points: story.points, created: story.created, due: story.due, start: story.started, end: story.finished, status: story.status, option: 'Edit' }
+      })
+  
+      dialogRef.afterClosed().subscribe(result => {
+  
+        if (result.value != undefined) {
+          const loading = this.dialog.open(LoadingDialogComponent)
+          this.storiesService.editStory(result.value, story.getId()).subscribe(resp => {
+            loading.close()
+            if (resp.status == "success") {
+              this.getStories();
+              this.dialog.open(DialogNotificationComponent, {
+                data: { title: "Success editing Storie: " + result.value.name, mensaje: "The storie has been edited" }
+              })
+            } else {
+              this.dialog.open(DialogNotificationComponent, {
+                data: { title: "Error editing Storie: " + result.value.name, mensaje: "Error in comunication with Database." }
+              })
+  
+            }
+          })
+        }
+      })
+    
+  }
+
+  updateProject(project: Project): void {
+    let dialogRef = this.dialog.open(ProjectDialogComponent, {
+      data: {
+        option: " Editar", name: project.getName(),
+        members: project.getMembers(), description: project.getDescription(),
+        icon: project.icon
+      }
+    });
+
+
+    dialogRef.afterClosed().subscribe(result => {
+      if (result.value != undefined){
+
+        const loading = this.dialog.open(LoadingDialogComponent)
+        //loading true
+        this.projectsService.editProject(result.value, project.getId()).subscribe(resp => {
+          loading.close()
+          if (resp.success = "success") {
+            this.getProjects();
+            //loading false
+            this.dialog.open(DialogNotificationComponent,{
+              data: { title: "Success editing Project", mensaje: "This project has been edited" }});
+          }else{
+            this.dialog.open(DialogNotificationComponent,{
+              data: { title: "Error editing Project", mensaje: "Error in comunication with Database" }});
+          }
+        });
+      }
+      
     })
   }
   
